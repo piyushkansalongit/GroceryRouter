dafdcc96226e6ab8faf6cdb7cb5200446847c468
Apoorv <japoorv0@gmail.com>
2020-04-11 18:31:51 +0530

    Fixes for vehicle_routing_problem API

--- app/src/androidTest/java/com/example/groceryrouter/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/groceryrouter/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.groceryrouter;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -8,6 +9,15 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import static android.content.Context.MODE_PRIVATE;
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static org.junit.Assert.*;
 
 /**
@@ -24,4 +34,51 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.groceryrouter", appContext.getPackageName());
     }
+    @Test
+    public void getResponse(){
+        String TESTSTRING = "lat_w,long_w,lat_d,long_d,cap_d,id_d,cap_v,id_v\n" +
+                "11,11,20,20,20,1,30,apoorv\n" +
+                "x,x,10,10,10,2,x,10\n" +
+                "0,0,x,x,10,10,10,2\n";
+        Log.d("test_string",TESTSTRING);
+        try {
+            File myfile = File.createTempFile("sample.csv",null,getApplicationContext().getCacheDir());
+            FileOutputStream fOut = new FileOutputStream(myfile);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            osw.write(TESTSTRING);
+            osw.close();
+            Log.d("file_loc",myfile.getAbsolutePath());
+            StringBuilder text = new StringBuilder();
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(myfile));
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    text.append(line);
+                    text.append('\n');
+                }
+
+                br.close();
+
+            }
+            catch (Exception e) {
+                Log.d("file_content","error");
+                //You'll need to add proper error handling here
+            }
+
+            Log.d("xx","xx");
+            Log.d("file_content",text.toString());
+            httpclient client = new httpclient();
+            client.getResponse(myfile.getAbsolutePath());
+        }
+        catch (IOException e)
+        {
+            Log.d("err_test","Unable to create file");
+        }
+
+    }
+
+
 }
+
--- app/src/main/java/com/example/groceryrouter/httpclient.java
+++ app/src/main/java/com/example/groceryrouter/httpclient.java
@@ -1,4 +1,168 @@
 package com.example.groceryrouter;
 
+
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.io.IOUtil;
+
+/**
+ * This example shows how to upload files using POST requests
+ * with encoding type "multipart/form-data".
+ * For more details please read the full tutorial
+ * on https://javatutorial.net/java-file-upload-rest-service
+ * @author javatutorial.net
+ */
 public class httpclient {
+    public static String getFileContent(
+            FileInputStream fis,
+            String          encoding ) throws IOException
+    {
+        try( BufferedReader br =
+                     new BufferedReader( new InputStreamReader(fis, encoding )))
+        {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while(( line = br.readLine()) != null ) {
+                sb.append( line );
+                sb.append( '\n' );
+            }
+            return sb.toString();
+        }
+    }
+
+    public static void getResponse(String name) {
+        try {
+            String sourceFileUri = name;
+            HttpURLConnection conn = null;
+            DataOutputStream dos = null;
+            String lineEnd = "\r\n";
+            String twoHyphens = "--";
+            String boundary = "*****";
+            int bytesRead, bytesAvailable, bufferSize;
+            byte[] buffer;
+            int maxBufferSize = 1 * 1024 * 1024;
+            File sourceFile = new File(sourceFileUri);
+            if (sourceFile.isFile()) {
+                try {
+                    String upLoadServerUri = "https://vehicleroutingproblem.herokuapp.com//handleUpload";
+
+                    // open a URL connection to the Servlet
+                    FileInputStream fileInputStream = new FileInputStream(
+                            sourceFile);
+                    URL url = new URL(upLoadServerUri);
+
+                    // Open a HTTP connection to the URL
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoInput(true); // Allow Inputs
+                    conn.setDoOutput(true); // Allow Outputs
+                    conn.setUseCaches(false); // Don't use a Cached Copy
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Connection", "Keep-Alive");
+                    conn.setRequestProperty("ENCTYPE",
+                            "multipart/form-data");
+                    conn.setRequestProperty("Content-Type",
+                            "multipart/form-data;boundary=" + boundary);
+                    conn.setRequestProperty("datax", sourceFileUri);
+
+                    dos = new DataOutputStream(conn.getOutputStream());
+
+                    dos.writeBytes(twoHyphens + boundary + lineEnd);
+                    dos.writeBytes("Content-Disposition: form-data; name=\"datax\";filename=\""
+                            + sourceFileUri + "\"" + lineEnd);
+
+                    dos.writeBytes(lineEnd);
+
+                    // create a buffer of maximum size
+                    bytesAvailable = fileInputStream.available();
+
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    buffer = new byte[bufferSize];
+
+                    // read file and write it into form...
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                    while (bytesRead > 0) {
+
+                        dos.write(buffer, 0, bufferSize);
+                        bytesAvailable = fileInputStream.available();
+                        bufferSize = Math
+                                .min(bytesAvailable, maxBufferSize);
+                        bytesRead = fileInputStream.read(buffer, 0,
+                                bufferSize);
+
+                    }
+
+                    // send multipart form data necesssary after file
+                    // data...
+                    dos.writeBytes(lineEnd);
+                    dos.writeBytes(twoHyphens + boundary + twoHyphens
+                            + lineEnd);
+
+                    // Responses from the server (code and message)
+                    int serverResponseCode = conn.getResponseCode();
+                    String serverResponseMessage = conn
+                            .getResponseMessage();
+                    Log.d("message",serverResponseMessage);
+                    Log.d("response-code",Integer.toString(serverResponseCode));
+                    try {
+                        InputStream in = new BufferedInputStream(conn.getInputStream());
+                        StringWriter writer = new StringWriter();
+                        IOUtil.copy(in, writer, "UTF-8");
+                        Log.d("Resonse",writer.toString());
+                    } finally {
+                        conn.disconnect();
+                    }
+
+                    if (serverResponseCode == 200) {
+
+                        // messageText.setText(msg);
+                        //Toast.makeText(ctx, "File Upload Complete.",
+                        //      Toast.LENGTH_SHORT).show();
+
+                        // recursiveDelete(mDirectory1);
+
+                    }
+
+                    // close the streams //
+                    fileInputStream.close();
+                    dos.flush();
+                    dos.close();
+
+                } catch (Exception e) {
+                    // dialog.dismiss();
+                    e.printStackTrace();
+                }
+                // dialog.dismiss();
+            } // End else block
+        } catch (Exception ex) {
+            // dialog.dismiss();
+            ex.printStackTrace();
+        }
+        return ;
+
+
+    }
+
+
+
+
+
+
+
 }
+
