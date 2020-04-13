285c2f520da867ba3226635786b0b83f82f3e00e
Apoorv <japoorv0@gmail.com>
2020-04-13 23:26:53 +0530

    Added API request in the starting of the app do reduce boot time

--- app/src/main/java/com/example/groceryrouter/WelcomeActivity.java
+++ app/src/main/java/com/example/groceryrouter/WelcomeActivity.java
@@ -5,6 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class WelcomeActivity extends AppCompatActivity {
 
 
@@ -20,6 +27,25 @@ public class WelcomeActivity extends AppCompatActivity {
         try {
             deliveryCoordinatesDB.deleteAll();
             deliveryAgentsDB.deleteAll();
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url ="https://vehicleroutingproblem.herokuapp.com/";
+
+// Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+
+// Add the request to the RequestQueue.
+            queue.add(stringRequest);
         }
         catch (Exception ignored){}
         Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
